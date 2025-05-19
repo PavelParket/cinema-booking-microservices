7f3ed06e90cae2061f169f269416a56a61b8ad70
Pavel Stankevich <pavelstankevich04@gmail.com>
2025-05-19 15:57:28 +0300

    Add discovery server

---
+++ discovery-server/src/test/java/com/booking/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
