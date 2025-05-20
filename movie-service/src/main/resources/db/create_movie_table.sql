d706c0b2e710bfffa552caef2e1ae634ed5dbe06
Pavel Stankevich <pavelstankevich04@gmail.com>
2025-05-20 11:48:21 +0300

    Initialize movie service

---
+++ movie-service/src/main/resources/db/create_movie_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS movie (
+   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(255) NOT NULL,
+   duration_minutes INTEGER NOT NULL,
+   director VARCHAR(255) NOT NULL,
+   release_date DATE NOT NULL,
+   country VARCHAR(255) NOT NULL,
+   age_rating INTEGER NOT NULL
+);
